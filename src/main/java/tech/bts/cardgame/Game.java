a86ea424e01cdd841b572f61b2c9a299b011cb80
Alessandro <alessandro.zaccolo@bts.tech>
2018-11-16 14:38:07 +0100

    Implemented picking cards and discarding

--- src/main/java/tech/bts/cardgame/Game.java
+++ src/main/java/tech/bts/cardgame/Game.java
@@ -1,87 +1,97 @@
 package tech.bts.cardgame;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Game {
 
+    public enum State { OPEN, PLAYING }
 
     private final Deck deck;
-    private String state;
+    private State state;
     private List<String> usernames;
+    private Map<String, Card> pickedCardByUsername;
 
     public Game(Deck deck) {
         this.deck = deck;
-        this.state = "open";
+        this.state = State.OPEN;
         this.usernames = new ArrayList<>();
+        this.pickedCardByUsername = new HashMap<>();
     }
 
-    public String getState() {
+    public State getState() {
         return state;
     }
 
+    public void join(String username) {
 
-    public void join(String username){
-
-        if (!state.equals("open")){
+        if (state != State.OPEN) {
             throw new JoiningNotAllowedException();
         }
+
         usernames.add(username);
 
-        if(usernames.size() == 2){
-            state = "playing";
+        if (usernames.size() == 2) {
+            state = State.PLAYING;
         }
-
     }
 
-    public List<String> getPlayerNames(){
+    public List<String> getPlayerNames() {
         return usernames;
     }
 
+    public Card pickCard(String username) {
 
-
-
-
-
-
-
-
-    // Returns a negative integer, sero, or a positive integer
-    // as the first hand is less than, equal to,
-    // or grater than the second hand.
-
-
-
-    /*
-
-    private static int compare(Hand hand1, Hand hand2){
-
-        int points1 = 0;
-        int points2 = 0;
-
-        Card total1 = hand1.calculate();
-        Card total2 = hand1.calculate();
-
-        if (total1.getMagic() > total2.getMagic()){
-            points1++;
-        } else if (total1.getMagic() < total2.getMagic()){
-            points2++;
+        if (!usernames.contains(username)) {
+            throw new PlayerNotInTheGameException();
         }
 
-        if (total1.getStrength() > total2.getStrength()){
-            points1++;
-        } else if (total1.getStrength() < total2.getStrength()){
-            points2++;
+        Card pickedCard = pickedCardByUsername.get(username);
+        if (pickedCard != null) {
+            throw new CannotPick2CardsInARowException();
         }
 
-        if (total1.getIntelligence() > total2.getIntelligence()){
-            points1++;
-        } else if (total1.getIntelligence() < total2.getIntelligence()){
-            points2++;
-        }
+        Card newPickedCard = deck.pickCard();
 
-        return points1 - points2;
+        pickedCardByUsername.put(username, newPickedCard);
 
+        return newPickedCard;
     }
-    */
+
+    public void discard(String username) {
+
+        pickedCardByUsername.remove(username);
+    }
+
+
+
+
+
+
+    /**
+     * Returns a negative integer, zero, or a positive integer
+     * as the first hand is less than, equal to,
+     * or greater than the second hand.
+     *
+     private int compare(Hand hand1, Hand hand2) {
+
+     int points1 = 0;
+     int points2 = 0;
+
+     Card total1 = hand1.calculate();
+     Card total2 = hand2.calculate();
+
+     if (total1.getMagic() > total2.getMagic()) {
+     points1++;
+     } else if (total1.getMagic() < total2.getMagic()) {
+     points2++;
+     }
+
+     // TODO: do the same with strength and intelligence
+
+     return points1 - points2;
+     }
+     */
 }
