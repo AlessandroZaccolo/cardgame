81b2c0e0e6acda63f6fd457f611330364eca3a55
Alessandro <alessandro.zaccolo@bts.tech>
2018-12-06 13:16:27 +0100

    Create games

---
+++ src/main/java/tech/bts/cardgame/model/Game.java
@@ -0,0 +1,137 @@
+package tech.bts.cardgame.model;
+
+import tech.bts.cardgame.exceptions.*;
+
+import java.util.*;
+
+public class Game {
+
+
+    public enum State { OPEN, PLAYING }
+
+
+    private long id;
+    private final Deck deck;
+    private State state;
+    private Map<String, Player> playersByUsername;
+
+    public Game(Deck deck) {
+        this.deck = deck;
+        this.state = State.OPEN;
+        this.playersByUsername = new HashMap<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void join(String username) {
+
+        if (state != State.OPEN) {
+            throw new JoiningNotAllowedException();
+        }
+
+        Player player = new Player(username);
+
+        playersByUsername.put(username, player);
+
+        if (playersByUsername.size() == 2) {
+            state = State.PLAYING;
+        }
+    }
+
+    public Set<String> getPlayerNames() {
+        return playersByUsername.keySet();
+    }
+
+    public Card pickCard(String username) {
+
+        if (state != State.PLAYING){
+            throw new NotPlayingException();
+        }
+
+        if (!playersByUsername.containsKey(username)) {
+            throw new PlayerNotInTheGameException();
+        }
+
+        Player player = playersByUsername.get(username);
+
+        Card pickedCard = player.getPickedCard();
+
+        if (pickedCard != null) {
+            throw new CannotPick2CardsInARowException();
+        }
+
+
+        Card newPickedCard = deck.pickCard();
+
+        player.setPickedCard(newPickedCard);
+
+        return newPickedCard;
+    }
+
+    public void discard(String username) {
+
+        Player player = playersByUsername.get(username);
+
+        Card pickedCard = player.getPickedCard();
+
+        if (pickedCard == null) {
+            throw new DidNotPickCardException();
+        }
+
+        int discards = player.getDiscardCount();
+
+        if(discards == 2){
+            throw new TooManyDiscardsException();
+        }
+
+        player.setDiscardCount(discards + 1);
+
+        player.setPickedCard(null);
+
+    }
+
+    /*@Override
+    public String toString(){
+        return "Game "+ id +
+    }
+    */
+
+
+
+
+
+
+    /**
+     * Returns a negative integer, zero, or a positive integer
+     * as the first hand is less than, equal to,
+     * or greater than the second hand.
+     *
+     private int compare(Hand hand1, Hand hand2) {
+
+     int points1 = 0;
+     int points2 = 0;
+
+     Card total1 = hand1.calculate();
+     Card total2 = hand2.calculate();
+
+     if (total1.getMagic() > total2.getMagic()) {
+     points1++;
+     } else if (total1.getMagic() < total2.getMagic()) {
+     points2++;
+     }
+
+
+     return points1 - points2;
+     }
+     */
+}
