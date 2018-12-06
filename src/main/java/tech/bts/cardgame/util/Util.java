74e38abd8530d2851fabed00af121366e37ff943
Alessandro <alessandro.zaccolo@bts.tech>
2018-12-06 13:18:26 +0100

    Util class (not finished)

---
+++ src/main/java/tech/bts/cardgame/util/Util.java
@@ -0,0 +1,16 @@
+package tech.bts.cardgame.util;
+
+import javafx.print.Collation;
+
+import java.util.Collection;
+
+public class Util {
+
+
+    public static String join(Collection<String> texts, String separate){
+
+        String result = "";
+
+        return result;
+    }
+}
