e4214fd5c265120d1428dc88c24666c591302101
alessandro.zaccolo <alessandro.zaccolo@bts.tech>
2019-02-18 13:06:26 +0100

    Add SQL database.

---
+++ src/main/resources/sql-scripts/sample-data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE games (
+id INT AUTO_INCREMENT PRIMARY KEY,
+state VARCHAR(50) NOT NULL,
+players VARCHAR (100)
+);
+
+insert into games (state, players) values
+  ('PLAYING', 'bart,lisa'),
+  ('OPEN', 'homer'),
+  ('OPEN', NULL);
